93f2273ea35012266bcba48a34eca084fc28fff9
nojin <nojin@LAPTOP-VTBSBR9C>
2023-08-23 14:35:09 +0900

    a

--- PBL2023_1/src/main/java/servlet/HelloMori.java
+++ PBL2023_1/src/main/java/servlet/HelloMori.java
@@ -3,6 +3,10 @@ package servlet;
 public class HelloMori {
 	public static void main(String[] args) {
 		System.out.println("Hello, Mori-san!");
+		
+		
+		
+		System.out.println("nojima!");
 	}
 
 }
