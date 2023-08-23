f24b8f341b4531d12b90ad559b4be452b4117aed
nojin <nojin@LAPTOP-VTBSBR9C>
2023-08-23 15:13:12 +0900

    a

--- PBL2023_1/src/main/java/servlet/HelloMori.java
+++ PBL2023_1/src/main/java/servlet/HelloMori.java
@@ -3,16 +3,8 @@ package servlet;
 public class HelloMori {
 	public static void main(String[] args) {
 		System.out.println("Hello, Mori-san!");
-		
-		
-		
-<<<<<<< HEAD
 		System.out.println("nojima!");
-=======
-		System.out.println("こんにちは！森さん！");
-		System.out.println("Additional message");
 		
->>>>>>> branch 'main' of https://github.com/takafumit/PBL2023_1.git
 	}
 
 }
